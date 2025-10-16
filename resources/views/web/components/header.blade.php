49861a3de7ec826f7752a2afe8c6f45c6ae00f6c
rahmat <rahmatbachtiar64@gmail.com>
2025-10-16 15:19:55 +0700

    Optimize seo

--- resources/views/web/components/header.blade.php
+++ resources/views/web/components/header.blade.php
@@ -44,7 +44,7 @@
 
 
 
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarCollapse">
